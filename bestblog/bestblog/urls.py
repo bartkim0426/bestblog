25491556cfb208c3ebcea9d1689254b9d8143368
Seul Chan Kim <seulchankim@Seului-MacBook-Pro.local>
2017-03-20 20:32:27 +0900

    Add home page  기본 홈페이지를 구상하였습니다.

--- bestblog/bestblog/urls.py
+++ bestblog/bestblog/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from bestblog.views import home
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', home, name='home'),
 ]
 
 if settings.DEBUG:
